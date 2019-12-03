018c1aa7ea5a7eef3b5b0e6ebe1c836ffd99d8b1
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-03 10:32:33 +0700

    Modified method index on Posts Controller Index

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    public function index(Post $post)
+    {
+        // Query sort by created_date
+        $posts = [];
+        $posts = $post::latest()->get();
+
+        // Bind query result to view
+        return view('post.index')->with(['posts' => $posts]);
+    }
+
 }
